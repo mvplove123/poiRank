234e056050fb105c2f2becf83b33c74fefbcb643
mvplove123 <mvplove123@163.com>
2017-08-03 13:52:16 +0800

    brandupdate done

--- src/main/scala/cluster/service/PoiService.scala
+++ src/main/scala/cluster/service/PoiService.scala
@@ -36,12 +36,12 @@ object PoiService {
       .point, x
       .city,
       x.category, x.subCategory,
-      x.brand, x.keyword, x.commentNum, x.price, x.grade, x.guid,x.alias).mkString("\t"))
+      x.brand, x.keyword, x.commentNum, x.price, x.grade, x.guid, x.alias).mkString("\t"))
     return poi
 
   }
 
-  def getPoiRdd(poiLine: RDD[String]): RDD[Poi] ={
+  def getPoiRdd(poiLine: RDD[String]): RDD[Poi] = {
     try {
       val line: RDD[Node] = poiLine.flatMap { s =>
         try {
@@ -56,14 +56,13 @@ object PoiService {
       val poiRdd: RDD[Poi] = line.map(x => parsePoi(x))
       return poiRdd
 
-    }catch {
+    } catch {
       case ex: Exception => {
       }
     }
     return null
 
   }
-
 
 
   def parsePoi(x: Node): Poi = {
@@ -83,13 +82,13 @@ object PoiService {
       poi.subCategory = srcType.apply(1)
     }
 
-    poi.keyword = x.\("KEYWORDS").text +","+ x.\("TAG").text
+    poi.keyword = x.\("KEYWORDS").text + "," + x.\("TAG").text
     poi.province = x.\("SRC_PROVINCE").text
     poi.brand = getBrand(poi.keyword)
     poi.geometry = x.\("GEOMETRY").text
     poi.alias = x.\("SRC_ALIAS").text
-    if(StringUtils.isBlank(poi.alias)){
-      poi.alias=" "
+    if (StringUtils.isBlank(poi.alias)) {
+      poi.alias = " "
     }
 
 
@@ -159,7 +158,7 @@ object PoiService {
               if (!(sellingPrice).isEmpty) poi.sellingPrice = featureValueToNumber(sellingPrice)
 
             case everythingElse =>
-                  None
+              None
           }
         }
 
@@ -201,26 +200,39 @@ object PoiService {
     * @return
     */
   private def getBrand(keyword: String): String = {
+//    if (StringUtils.isBlank(keyword)) {
+//      return " "
+//    }
+    val tagLSBrand: String = ".*LS:(.*)"
+    val tagALSBrand: String = ".*ALS:(.*)"
+
+    var brandSet: Set[String] = Set[String]()
+
+    val pLsTagBrand: Pattern = Pattern.compile(tagLSBrand)
+    val mLsTagBrand: Matcher = pLsTagBrand.matcher(keyword)
+
+    val pAlsTagBrand: Pattern = Pattern.compile(tagALSBrand)
+    val mAlsTagBrand: Matcher = pAlsTagBrand.matcher(keyword)
+
+
+    if (mLsTagBrand.matches) {
+      val currentBrand: String = mLsTagBrand.group(1)
+      val end: Int = currentBrand.indexOf("$")
+      val brand: String = currentBrand.substring(0, end)
+      brandSet = brandSet.+(brand)
+    }
+    if (mAlsTagBrand.matches()) {
+      val currentBrand: String = mAlsTagBrand.group(1)
+      val end: Int = currentBrand.indexOf("$")
+      val brand: String = currentBrand.substring(0, end)
+      brandSet = brandSet.+(brand)
+    }
+
+    val brands = brandSet.mkString(",")
     if (StringUtils.isBlank(keyword)) {
       return " "
     }
-
-
-    val tagBrand: String = ".*LS:(.*)"
-
-    //    val tagBrand: String = "(?<=LS:)[^$]+"
-
-    val pTagBrand: Pattern = Pattern.compile(tagBrand)
-    val mTagBrand: Matcher = pTagBrand.matcher(keyword)
-    if (mTagBrand.matches) {
-      val currentBrand: String = mTagBrand.group(1)
-      val end: Int = currentBrand.indexOf("$")
-      val brand: String = currentBrand.substring(0, end)
-      return brand
-    }
-    else {
-      return " "
-    }
+    return brands
   }
 
 
@@ -236,8 +248,13 @@ object PoiService {
 
 
   def main(args: Array[String]) {
-    val keyword: String = "$$TP:TP10006$$,$$DFB:美团团购$$,$$DFB:美团团购-餐饮团购$$,$$DFB:餐饮美食$$,$$DFB:餐饮美食-快餐小吃$$,$$DG:点评美食-快餐简餐$$,$$FB:餐饮美食-快餐小吃-中式快餐-云南过桥米线-快餐$$,$$LS:云南过桥米线$$,$$RC:云南过桥米线-云南过桥米线馆-云南过桥米线坊-云南过桥米线饭庄-云南过桥米线麻辣烫-云南过桥米线家和店-云南过桥米线滇城小镇-云南过桥米线麻辣土豆粉-云南过桥米线/黄焖鸡米饭-云南过桥米线老四川麻辣烫$$,$$RC:其它快餐小吃$$"
-    val brand = getBrand(keyword = keyword)
+    val keyword: String = "$$TP:TP10006$$,$$DFB:美团团购$$,$$ALS:云南过桥米线$$,$$DFB:美团团购-餐饮团购$$,$$DFB:餐饮美食$$," +
+      "$$DFB:餐饮美食-快餐小吃$$," +
+      "$$DG:点评美食-快餐简餐$$,$$FB:餐饮美食-快餐小吃-中式快餐-云南过桥米线-快餐$$,$$LS:云南过桥米线$$,$$RC:云南过桥米线-云南过桥米线馆-云南过桥米线坊-云南过桥米线饭庄-云南过桥米线麻辣烫-云南过桥米线家和店-云南过桥米线滇城小镇-云南过桥米线麻辣土豆粉-云南过桥米线/黄焖鸡米饭-云南过桥米线老四川麻辣烫$$,$$RC:其它快餐小吃$$"
+
+    val key = ""
+
+    val brand = getBrand(keyword = key)
 
     print(brand)
   }
